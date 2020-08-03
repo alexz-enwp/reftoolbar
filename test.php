4997b5af39660748c8c40caadaecf407caad941e
Kaldari <rkaldari@wikimedia.org>
2020-08-03 15:19:23 -0400

    clean-up recent changes

--- test.php
+++
@@ -1,6 +0,0 @@
-<?php
-$value = "Third edition";
-$result['edition'] = str_replace(' edition','',$value);
-$result['edition'] = str_replace(' ed.','',$value);
-$result['edition'] = str_replace(' ed','',$value);
-echo $result['edition'];
